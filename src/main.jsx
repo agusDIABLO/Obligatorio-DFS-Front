224fe67c8ea9b0c7c1e107deeb8d8e11fb57e991
agusDIABLO <qewad1@cloud.com>
2025-11-10 10:21:09 -0300

    commit inicial

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./redux/stores.js";
+import { Provider } from "react-redux";
+import Rutas from "./routes/Routes.jsx";
+import { ToastContainer } from "react-toastify";
+import "./i18n.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ToastContainer />
+    <Rutas></Rutas>
+  </Provider>
+);
+
