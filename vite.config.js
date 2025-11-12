1d7bff904a4529aedd9a2db96464701b3f2d1e19
agusDIABLO <qewad1@cloud.com>
2025-11-12 01:31:28 -0300

    cambio en vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, port: 5176,
-    watch: { usePolling: true }
-  }
-})
+    proxy: {
+      '/api': {
+        target: 'https://obligatorio-dfs-inky.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+      },
+    },
+  },
+});
